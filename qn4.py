a8da8c46c5abcd342f9571ba7489927f5cd31ecf
beachaye <beach@gmx.us>
2019-02-01 19:00:33 +0300

    qn4 done

---
+++ qn4.py
@@ -0,0 +1,17 @@
+#Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+#Suppose the following input is supplied to the program:
+#34,67,55,33,12,98
+#Then, the output should be:
+#['34', '67', '55', '33', '12', '98']
+#('34', '67', '55', '33', '12', '98')
+
+l = input("Enter Sequence: ")
+z = []
+
+for i in l.split(","):
+    z.append(int(i))
+
+print(z)
+print(tuple(z))
+
+
